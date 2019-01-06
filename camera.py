46d555e94786044de0756848b928af57e451b214
kulvirk <kulvirk@users.noreply.github.com>
2019-01-06 19:42:18 +0530

    Create camera.py

---
+++ camera.py
@@ -0,0 +1,17 @@
+from time import strftime 
+import time
+from datetime import datetime 
+from face_detect import find_face 
+import time,os, sys,cv2 
+cascadePath ="haarcascade_frontalface_default.xml" 
+image_to_detect="subject95.jpg"
+
+def take_pic():
+    Ntime=datetime.now().strftime('%b,%d,%H:%M:%S')
+    camera="sudo fswebcam -S 2 -r 640x480 newface/subject95.jpg subject95.jpg "
+    os.system(camera)
+    time.sleep(1)
+    photo=cv2.imread(image_to_detect)
+    cv2.imshow("you are",photo)
+    cv2.waitKey(1000)
+       
