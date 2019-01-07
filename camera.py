1a0d16f75b6e011b945335d25252cc29d16b543c
kulvirk <kulvirk@users.noreply.github.com>
2019-01-07 09:31:28 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,11 +4,11 @@ from datetime import datetime
 from face_detect import find_face 
 import time,os, sys,cv2 
 cascadePath ="haarcascade_frontalface_default.xml" 
-image_to_detect="subject95.jpg"
+image_to_detect="face95.jpg"
 
 def take_pic():
     Ntime=datetime.now().strftime('%b,%d,%H:%M:%S')
-    camera="sudo fswebcam -S 2 -r 640x480 newface/subject95.jpg subject95.jpg "
+    camera="sudo fswebcam -S 2 -r 640x480 newface/face95.jpg face95.jpg "
     os.system(camera)
     time.sleep(1)
     photo=cv2.imread(image_to_detect)
